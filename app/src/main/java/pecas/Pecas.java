72d9bba287a8f3e6bbd7f1f2939ae380514dc7a3
Gabriel Silva <gabrielhenriquesilva002@gmail.com>
2024-11-18 17:06:57 -0300

    Classe abstrata peças criada

--- app/src/main/java/pecas/Pecas.java
+++ app/src/main/java/pecas/Pecas.java
@@ -1,5 +1,34 @@
 package pecas;
+import partida.*;
 
-public interface Pecas {
+public abstract class Pecas {
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
 
+    public Pecas(Posicao posicao, Tabuleiro tabuleiro) {
+        this.posicao = posicao;
+        this.tabuleiro = tabuleiro;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public abstract boolean[][] movPossiveis();
+
+    public boolean movPossivel(Posicao posicao){
+        return movPossiveis()[posicao.getLinha()][posicao.getColuna()];
+    }
+
+    public boolean ePossivelMover(){
+        boolean[][] move = movPossiveis();
+        for (int i = 0; i < move.length; i++) {
+			for (int j = 0; j < move.length; j++) {
+				if (move[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+    }
 }
