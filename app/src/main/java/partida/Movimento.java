0af8a1b8da655d802917babe90e54c2561c99990
jgfpedra <j248349@dac.unicamp.br>
2024-11-18 19:18:40 -0300

    Logica: Mudanca no retorno de Peca | Implementeacao da logica de movimento da classe Bispo | Implementacao da classe Movimento

--- app/src/main/java/partida/Movimento.java
+++ app/src/main/java/partida/Movimento.java
@@ -1,5 +1,33 @@
 package partida;
 
-public class Movimento {
+import java.util.List;
 
+import pecas.Peca;
+
+public class Movimento {
+    private Posicao origem;
+    private Posicao destino;
+    private Peca pecaMovida;
+
+    public Movimento(Posicao origem, Posicao destino, Peca pecaMovida){
+        this.origem = origem;
+        this.pecaMovida = pecaMovida;
+
+        List<Posicao> destValidos = pecaMovida.proxMovimento(origem);
+
+        if (destValidos.contains(destino)) {
+            this.destino = destino;  // If valid, set the destination
+        } else {
+            throw new IllegalArgumentException("Invalid move: The destination is not valid for this piece.");
+        }
+    }
+    public Posicao getOrigem() {
+        return origem;
+    }
+    public Posicao getDestino() {
+        return destino;
+    }
+    public Peca getPecaMovida() {
+        return pecaMovida;
+    }
 }
--- app/src/main/java/pecas/Bispo.java
+++ app/src/main/java/pecas/Bispo.java
@@ -1,19 +1,40 @@
 package pecas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.image.Image;
 import partida.Cor;
-import partida.Movimento;
 import partida.Posicao;
 
-public class Bispo extends Pecas{
+public class Bispo extends Peca{
 
     public Bispo(Cor cor, Image imagem){
         super(cor, imagem);
     }
     @Override
-    public Movimento proxMovimento(Posicao origem) {
-        // TODO: a partir de um objeto Movimento, a gente verifica as casas
-        throw new UnsupportedOperationException("Unimplemented method 'proxMovimento'");
+    public List<Posicao> proxMovimento(Posicao origem) {
+        List<Posicao> movimentosValidos = new ArrayList<>();
+        // Diagonal movements (top-left, top-right, bottom-left, bottom-right)
+        for (int i = 1; i < 8; i++) {
+            // Top-left diagonal
+            if (origem.getLinha() - i >= 0 && origem.getColuna() - i >= 0) {
+                movimentosValidos.add(new Posicao(origem.getLinha() - i, origem.getColuna() - i));
+            }
+            // Top-right diagonal
+            if (origem.getLinha() - i >= 0 && origem.getColuna() + i < 8) {
+                movimentosValidos.add(new Posicao(origem.getLinha() - i, origem.getColuna() + i));
+            }
+            // Bottom-left diagonal
+            if (origem.getLinha() + i < 8 && origem.getColuna() - i >= 0) {
+                movimentosValidos.add(new Posicao(origem.getLinha() + i, origem.getColuna() - i));
+            }
+            // Bottom-right diagonal
+            if (origem.getLinha() + i < 8 && origem.getColuna() + i < 8) {
+                movimentosValidos.add(new Posicao(origem.getLinha() + i, origem.getColuna() + i));
+            }
+        }
+        // Assuming Movimento can accept a list of valid positions
+        return movimentosValidos;
     }
-
 }
