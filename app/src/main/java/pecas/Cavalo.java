d8f35251a87e1670d5d3e0a42b5e5059082302fa
jgfpedra <j248349@dac.unicamp.br>
2024-11-18 19:21:25 -0300

    Logica: implementacao classe Cavalo

--- app/src/main/java/pecas/Cavalo.java
+++ app/src/main/java/pecas/Cavalo.java
@@ -1,5 +1,36 @@
 package pecas;
 
-public class Cavalo {
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.scene.image.Image;
+import partida.Cor;
+import partida.Posicao;
+
+public class Cavalo extends Peca {
+
+    public Cavalo(Cor cor, Image imagem) {
+        super(cor, imagem);
+    }
+
+    @Override
+    public List<Posicao> proxMovimento(Posicao origem) {
+        List<Posicao> movimentosValidos = new ArrayList<>();
+        // All possible L-shaped moves for the Cavalo
+        int[][] direcoes = {
+            {2, 1}, {2, -1}, {-2, 1}, {-2, -1},  // Two squares in one direction, one square perpendicular
+            {1, 2}, {1, -2}, {-1, 2}, {-1, -2}   // One square in one direction, two squares perpendicular
+        };
+        // Iterate through all possible L-shaped moves
+        for (int[] dir : direcoes) {
+            int novaLinha = origem.getLinha() + dir[0];
+            int novaColuna = origem.getColuna() + dir[1];
+
+            // Check if the move is within the bounds of the board (0 to 7)
+            if (novaLinha >= 0 && novaLinha < 8 && novaColuna >= 0 && novaColuna < 8) {
+                movimentosValidos.add(new Posicao(novaLinha, novaColuna));
+            }
+        }
+        return movimentosValidos;
+    }
 }
