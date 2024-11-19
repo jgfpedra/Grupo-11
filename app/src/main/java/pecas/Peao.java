4e4b88daf06fb89fc80eedbfb55358c307446ab3
Gabriel Silva <gabrielhenriquesilva002@gmail.com>
2024-11-18 21:50:02 -0300

    Peao: lista de movimentos

--- app/src/main/java/pecas/Peao.java
+++ app/src/main/java/pecas/Peao.java
@@ -1,5 +1,33 @@
 package pecas;
 
-public class Peao {
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.scene.image.Image;
+import partida.*;
+
+public class Peao extends Peca {
+
+    public Peao(Cor cor, Image imagem) {
+        super(cor, imagem);
+    }
+
+    @Override
+    public List<Posicao> proxMovimento(Posicao origem){
+        List<Posicao> movimentosValidos = new ArrayList<>();
+        
+        if (getMovCount() == 0) { // Primeiro movimento do peão pode ser de 2 casas para frente
+            int novaLinha = origem.getLinha() + 2;
+            int novaColuna = origem.getColuna() + 0;
+            movimentosValidos.add(new Posicao(novaLinha, novaColuna));
+        }
+        else { // Demais movimentos apenas 1 casa para frente
+            int novaLinha = origem.getLinha() + 1;
+            int novaColuna = origem.getColuna() + 0;
+            movimentosValidos.add(new Posicao(novaLinha, novaColuna));
+        }
+        if () { // 
+            
+        }
+    }
 }
