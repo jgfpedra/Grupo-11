cefe544dff8d4ba88a7e68c947d439bb74f66bf4
jgfpedra <j248349@dac.unicamp.br>
2024-11-18 21:14:09 -0300

    Logica: reestrturacao classes Jogador - mudanca para abstract

--- app/src/main/java/jogador/JogadorOnline.java
+++ app/src/main/java/jogador/JogadorOnline.java
@@ -1,31 +1,21 @@
 package jogador;
 
+import javafx.scene.image.Image;
+import partida.Cor;
 import partida.Tabuleiro;
 
-public class JogadorOnline implements Jogador{
+public class JogadorOnline extends Jogador{
 
+    public JogadorOnline(){
+
+    }
+    public JogadorOnline(Cor cor, String nome, Image imagem){
+        super(cor, nome, imagem);
+    }
     @Override
     public void escolherMovimento(Tabuleiro tabuleiro) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'escolherMovimento'");
     }
-
     @Override
     public void temPecas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'temPecas'");
     }
-
-    @Override
-    public void getCor() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCor'");
-    }
-
-    @Override
-    public void getNome() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
-    }
-
 }
