9b5766e7b6866e33140215fe897b8c453bdb7766
jgfpedra <j248349@dac.unicamp.br>
2024-11-18 21:01:18 -0300

    Logica: implementacao estrutura Jogadores

--- app/src/main/java/jogador/JogadorIA.java
+++ app/src/main/java/jogador/JogadorIA.java
@@ -1,5 +1,30 @@
 package jogador;
 
-public class JogadorIA {
+import partida.Tabuleiro;
 
+public class JogadorIA implements Jogador{
+
+    @Override
+    public void escolherMovimento(Tabuleiro tabuleiro) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'escolherMovimento'");
+    }
+
+    @Override
+    public void temPecas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'temPecas'");
+    }
+
+    @Override
+    public void getCor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCor'");
+    }
+
+    @Override
+    public void getNome() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
+    }
 }
--- app/src/main/java/jogador/JogadorLocal.java
+++ app/src/main/java/jogador/JogadorLocal.java
@@ -1,5 +1,31 @@
 package jogador;
 
-public class JogadorLocal {
+import partida.Tabuleiro;
+
+public class JogadorLocal implements Jogador {
+
+    @Override
+    public void escolherMovimento(Tabuleiro tabuleiro) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'escolherMovimento'");
+    }
+
+    @Override
+    public void temPecas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'temPecas'");
+    }
+
+    @Override
+    public void getCor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCor'");
+    }
+
+    @Override
+    public void getNome() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
+    }
 
 }
