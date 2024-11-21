5bda4b2cd0ee6f18245531fc4ca32f329f1803c5
jgfpedra <j248349@dac.unicamp.br>
2024-11-21 18:05:32 -0300

    Logica: Configuracao Imagens temporarias - para continuar testes

--- app/src/main/java/jogador/Jogador.java
+++ app/src/main/java/jogador/Jogador.java
@@ -26,8 +26,9 @@ public abstract class Jogador {
     }
     public abstract void escolherMovimento(Tabuleiro tabuleiro);
     public abstract void temPecas();
-    private static Image carregarImagem(Cor cor, String tipoPeca) {
-        String caminhoImagem = "/images/jogadores/" + tipoPeca + "_" + (cor == Cor.BRANCO ? "branco" : "preto") + ".png";
+    private static Image carregarImagem(Cor cor, String tipoJogador) {
+        System.out.println(tipoJogador);
+        String caminhoImagem = "/images/jogadores/" + tipoJogador + ".png";
         return new Image(Jogador.class.getResourceAsStream(caminhoImagem));
     }
 }
