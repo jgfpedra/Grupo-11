9c6cb321ec492d43298a02c17a0150ad967a6066
jgfpedra <j248349@dac.unicamp.br>
2024-11-21 18:05:32 -0300

    Logica: Configuracao Imagens temporarias - para continuar testes

--- app/src/main/java/xadrez/Main.java
+++ app/src/main/java/xadrez/Main.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import jogador.Jogador;
