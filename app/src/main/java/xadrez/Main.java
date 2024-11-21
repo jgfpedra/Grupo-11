02d608be6060d611b6b190d2fd701a85748a9241
jgfpedra <j248349@dac.unicamp.br>
2024-11-21 06:06:44 -0300

    UI: Adicao das imagens e mudanca do tamanho do tabuleiro

--- app/src/main/java/xadrez/Main.java
+++ app/src/main/java/xadrez/Main.java
@@ -83,7 +83,7 @@ public class Main extends Application{
         
         // Exibir a cena do tabuleiro
         primaryStage.setTitle("Jogo de Xadrez");
-        primaryStage.setScene(new Scene(tabuleiroView, 640, 640));
+        primaryStage.setScene(new Scene(tabuleiroView, 2048, 2048));
         primaryStage.show();
     }    
 }
