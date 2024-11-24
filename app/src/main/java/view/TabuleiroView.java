c2ed5fba06ab5d4af55a680cc3425ba53e79716f
jgfpedra <j248349@dac.unicamp.br>
2024-11-24 17:57:51 -0300

    UI: Consertado adicao de peca sem ser na captura

--- app/src/main/java/view/TabuleiroView.java
+++ app/src/main/java/view/TabuleiroView.java
@@ -21,22 +21,17 @@ import java.util.List;
 import java.util.Map;
 
 public class TabuleiroView extends GridPane {
-    public static final int TILE_SIZE = 80;  // Tamanho de cada célula no tabuleiro
+    public static final int TILE_SIZE = 80;
     private Partida partida;
     private Tabuleiro tabuleiro;
-    private Map<Posicao, ImageView> pecasNoTabuleiro; // Mapeia posições para ImageViews
+    private Map<Posicao, ImageView> pecasNoTabuleiro;
     private Peca pecaSelecionada;
     private Posicao posicaoSelecionada;
-    
-    // Imagens dos jogadores
-    private Image player1Image;
-    private Image player2Image;
-    
-    // Label para exibir o estado do jogo
+    private Image imagemJogadorPreto;
+    private Image imagemJogadorBranco;
     private Label estadoJogoLabel;
-
-    private HBox capturasJogador1;  // Caixa para as capturas do jogador 1
-    private HBox capturasJogador2;  // Caixa para as capturas do jogador 2
+    private HBox capturasJogadorPreto;
+    private HBox capturasJogadorBranco;
 
 
     // Construtor
@@ -47,30 +42,6 @@ public class TabuleiroView extends GridPane {
         desenharTabuleiro();
         inicializarEstadoJogoLabel();
         inicializarCapturasJogadores();
-    }
-
-    private void carregarImagensJogadores() {
-        // Carregar imagens dos jogadores (usando o caminho correto)
-        player1Image = new Image(getClass().getResourceAsStream("/images/jogadores/jogadorlocal.png"));
-        player2Image = new Image(getClass().getResourceAsStream("/images/jogadores/jogadorlocal.png"));
-    }
-
-    private void inicializarCapturasJogadores(){
-        capturasJogador1 = new HBox(10);
-        capturasJogador1.setStyle("-fx-padding: 10px; -fx-border-color: black;");
-        capturasJogador1.setPrefHeight(100);
-        capturasJogador2 = new HBox(10);
-        capturasJogador2.setStyle("-fx-padding: 10px; -fx-border-color: black;");
-        capturasJogador2.setPrefHeight(100);
-        add(capturasJogador1, 0, 9, 8, 1);
-        add(capturasJogador2, 0, 10, 8, 1);
-    }
-
-    private void inicializarEstadoJogoLabel() {
-        estadoJogoLabel = new Label("Vez do Jogador 1");
-        estadoJogoLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
-        estadoJogoLabel.setTextFill(Color.BLACK);
-        add(estadoJogoLabel, 0, 10, 8, 1);
     }
 
     public void updateTabuleiro(Tabuleiro tabuleiro) {
@@ -90,9 +61,10 @@ public class TabuleiroView extends GridPane {
             }
         }
         updateEstadoJogo(partida.getEstadoJogo().toString());
-        atualizarCapturasJogador1(partida.getTabuleiro().getCapturadasJogador1());
-        atualizarCapturasJogador2(partida.getTabuleiro().getCapturadasJogador2());
-    } 
+        atualizarCapturasJogador(capturasJogadorPreto, partida.getTabuleiro().getCapturadasJogadorPreto());
+        atualizarCapturasJogador(capturasJogadorBranco, partida.getTabuleiro().getCapturadasJogadorBranco());
+    }
+
     public void desenharTabuleiro() {
         getChildren().clear();
         for (int linha = 0; linha < 8; linha++) {
@@ -115,19 +87,67 @@ public class TabuleiroView extends GridPane {
             }
         }
     }
+
+    public void selecionarPeca(Posicao posicao) {
+        Peca peca = tabuleiro.obterPeca(posicao);
+        if (peca != null && peca.getCor() == partida.getJogadorAtual().getCor()) {
+            this.pecaSelecionada = peca;
+            this.posicaoSelecionada = posicao;
+        }
+    }
+
+    public void moverPeca(Posicao destino) {
+        if (pecaSelecionada != null) {
+            List<Posicao> movimentosPossiveis = pecaSelecionada.proxMovimento(posicaoSelecionada);
+            if (movimentosPossiveis.contains(destino)) {
+                animarMovimento(posicaoSelecionada, destino);   
+                Movimento movimentoAtual = new Movimento(posicaoSelecionada, destino, pecaSelecionada);  // Usando a pecaSelecionada
+                partida.jogar(movimentoAtual);
+                clearSelection();
+            } else {
+                clearSelection();
+            }
+        }
+    }
+
+    private void carregarImagensJogadores() {
+        // Carregar imagens dos jogadores (usando o caminho correto)
+        imagemJogadorPreto = new Image(getClass().getResourceAsStream("/images/jogadores/jogadorlocal.png"));
+        imagemJogadorBranco = new Image(getClass().getResourceAsStream("/images/jogadores/jogadorlocal.png"));
+    }
+
+    private void inicializarEstadoJogoLabel() {
+        estadoJogoLabel = new Label("Vez do Jogador 1");
+        estadoJogoLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        estadoJogoLabel.setTextFill(Color.BLACK);
+        add(estadoJogoLabel, 0, 10, 8, 1);
+    }
+
+    private void inicializarCapturasJogadores(){
+        capturasJogadorPreto = new HBox(10);
+        capturasJogadorPreto.setStyle("-fx-padding: 10px; -fx-border-color: black;");
+        capturasJogadorPreto.setPrefHeight(100);
+        capturasJogadorBranco = new HBox(10);
+        capturasJogadorBranco.setStyle("-fx-padding: 10px; -fx-border-color: black;");
+        capturasJogadorBranco.setPrefHeight(100);
+        add(capturasJogadorPreto, 0, 9, 8, 1);
+        add(capturasJogadorBranco, 0, 10, 8, 1);
+    }
+
     private void adicionarImagensJogadores() {
         carregarImagensJogadores();
         // Coloca as imagens dos jogadores no topo do tabuleiro
-        ImageView player1ImageView = new ImageView(player1Image);
+        ImageView player1ImageView = new ImageView(imagemJogadorPreto);
         player1ImageView.setFitWidth(50);
         player1ImageView.setFitHeight(50);
         add(player1ImageView, 8, 0);
 
-        ImageView player2ImageView = new ImageView(player2Image);
+        ImageView player2ImageView = new ImageView(imagemJogadorBranco);
         player2ImageView.setFitWidth(50);
         player2ImageView.setFitHeight(50);
         add(player2ImageView, 8, 7);
     }
+
     private void adicionarRegua() {
         for (int i = 0; i < 8; i++) {
             Text colLabel = new Text(String.valueOf(8 - i));
@@ -138,39 +158,13 @@ public class TabuleiroView extends GridPane {
             add(rowLabel, 8, i);
         }
     }
+
     public void adicionarPecaNoTabuleiro(Posicao posicao, Peca peca) {
         ImageView imgView = new ImageView(peca.getImage());
         imgView.setFitWidth(TILE_SIZE);
         imgView.setFitHeight(TILE_SIZE);
         add(imgView, posicao.getColuna(), posicao.getLinha());
         pecasNoTabuleiro.put(posicao, imgView);
-    }
-
-    public void selecionarPeca(Posicao posicao) {
-        Peca peca = tabuleiro.obterPeca(posicao);
-        if (peca != null && peca.getCor() == partida.getJogadorAtual().getCor()) {
-            this.pecaSelecionada = peca;
-            this.posicaoSelecionada = posicao;
-        }
-    }
-
-    // Método para mover a peça
-    public void moverPeca(Posicao destino) {
-        if (pecaSelecionada != null) {  // Verifica se a peça foi selecionada
-            List<Posicao> movimentosPossiveis = pecaSelecionada.proxMovimento(posicaoSelecionada);
-            if (movimentosPossiveis.contains(destino)) {
-                animarMovimento(posicaoSelecionada, destino);
-                Movimento movimentoPeca = new Movimento(posicaoSelecionada, destino, pecaSelecionada);
-                tabuleiro.aplicarMovimento(movimentoPeca);
-                updateTabuleiro(tabuleiro);
-                clearSelection();
-            } else {
-                System.out.println("Movimento inválido.");
-                clearSelection();  // Limpar seleção caso o movimento não seja válido
-            }
-        } else {
-            System.out.println("Nenhuma peça selecionada.");
-        }
     }
 
     public void clearSelection() {
@@ -229,13 +223,6 @@ public class TabuleiroView extends GridPane {
             transicao.setCycleCount(1);
             transicao.setDuration(Duration.seconds(0.5));
             transicao.play();
-    
-            // Após a animação, atualizar a posição e a UI
-            transicao.setOnFinished(event -> {
-                pecasNoTabuleiro.put(destino, pecaImagem);  // Adiciona a peça ao novo destino
-                add(pecaImagem, destino.getColuna(), destino.getLinha());  // Coloca na nova posição na UI
-                clearHighlights();  // Limpa os destaques
-            });
         }
     }    
 
@@ -243,23 +230,15 @@ public class TabuleiroView extends GridPane {
         estadoJogoLabel.setText(estadoJogo);  // Atualiza o texto do Label com o novo estado do jogo
     }
 
-    public void atualizarCapturasJogador1(List<Peca> capturadas) {
-        capturasJogador1.getChildren().clear();
+    public void atualizarCapturasJogador(HBox capturadasJogador, List<Peca> capturadas) {
+        capturadasJogador.getChildren().clear();  // Limpa a área de capturas do jogador 1
+        System.out.println(capturadas);
         for (Peca peca : capturadas) {
+            System.out.println("vrnc");
             ImageView imageView = new ImageView(peca.getImage());
-            imageView.setFitWidth(50);  // Definindo a largura da imagem
-            imageView.setPreserveRatio(true); // Mantendo a proporção original
-            capturasJogador1.getChildren().add(imageView);
+            imageView.setFitWidth(50);  // Define a largura da imagem
+            imageView.setPreserveRatio(true); // Mantém a proporção da imagem
+            capturadasJogador.getChildren().add(imageView);  // Adiciona a imagem à área de capturas
         }
-    }
-    
-    public void atualizarCapturasJogador2(List<Peca> capturadas) {
-        capturasJogador2.getChildren().clear();
-        for (Peca peca : capturadas) {
-            ImageView imageView = new ImageView(peca.getImage());
-            imageView.setFitWidth(50);  // Definindo a largura da imagem
-            imageView.setPreserveRatio(true); // Mantendo a proporção original
-            capturasJogador2.getChildren().add(imageView);
-        }
-    }
+    }    
 }
